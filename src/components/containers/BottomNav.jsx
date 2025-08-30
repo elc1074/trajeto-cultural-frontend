3a7ac8ecd18de98c6571d1801cd31010c74bb473
Dionatan Rodrigues <dionatanrodrigues351@gmail.com>
2025-08-30 02:31:14 -0300

    ✨ feat : add navigation function on the navbar

--- src/components/containers/BottomNav.jsx
+++ src/components/containers/BottomNav.jsx
@@ -5,28 +5,36 @@ import {
   BookOpenText,
   Delete,
 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export function BottomNav() {
+  const navigate = useNavigate();
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-purple-800">
       <nav className="flex justify-around items-center h-16">
-        <button className="text-white text-2xl p-2">
+        <button
+          className="text-white text-2xl p-2 hover:cursor-pointer"
+          onClick={() => navigate("/home")}
+        >
           <Home />
         </button>
 
-        <button className="text-white text-2xl p-2">
+        <button className="text-white text-2xl p-2 hover:cursor-pointer">
           <BatteryCharging />
         </button>
 
-        <button className="text-white text-2xl p-2">
+        <button
+          className="text-white text-2xl p-2 hover:cursor-pointer"
+          onClick={() => navigate("/ponto-artistico")}
+        >
           <MapPin />
         </button>
 
-        <button className="text-white text-2xl p-2">
+        <button className="text-white text-2xl p-2 hover:cursor-pointer" onClick={() => navigate("/eventos-details")}>
           <BookOpenText />
         </button>
 
-        <button className="text-white text-2xl p-2">
+        <button className="text-white text-2xl p-2 hover:cursor-pointer">
           <Delete />
         </button>
       </nav>

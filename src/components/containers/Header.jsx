befc56255821d82f7d535fcab73beca16eea07e6
Dionatan Rodrigues <dionatanrodrigues351@gmail.com>
2025-08-30 02:28:30 -0300

    ♻️ refactor : style made with Tailwind

--- src/components/containers/Header.jsx
+++ src/components/containers/Header.jsx
@@ -1,21 +1,17 @@
 import { MapPin } from "lucide-react";
 
-import "./css/Header.css"
-
 export function Header({ section }) {
   return (
     <header className="w-full shadow-md">
       {/* Barra superior */}
-      <div className="flex items-center gap-2 bg-color px-4 py-2">
-        <MapPin className="h-5 w-5 bg-color" color="white" />
-        <span className="font-semibold bg-color text-color-white">
-          Trajeto Cultural
-        </span>
+      <div className="flex items-center gap-2 bg-[#A457C7] px-4 py-6">
+        <MapPin className="h-5 w-5" color="white" />
+        <span className="font-semibold text-white">Trajeto Cultural</span>
       </div>
 
       {/* Barra inferior */}
-      <div className="bg-color-secundary px-4 py-3 border-b">
-        <h1 className="bg-color-secundary text-color-white text-center text-lg font-medium">
+      <div className="bg-[#C79CD1] px-4 py-3 border-b">
+        <h1 className="text-white text-center text-lg font-medium">
           {section}
         </h1>
       </div>

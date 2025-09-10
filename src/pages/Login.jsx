f6caf5e1c17709743517ddbf784e030ef6232f64
Vitória Camara <vlcamara@inf.ufsm.br>
2025-09-10 08:55:35 -0300

    feat: add loader button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,14 +5,15 @@ import { UserContext } from "../context/UserContext";
 
 const Login = () => {
   const navigate = useNavigate();
-
   const { login } = useContext(UserContext);
 
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [erro, setErro] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
+    setLoading(true);
     try {
       const response = await fetch("https://trajeto-cultural-backend.onrender.com/usuario/login", {
         method: "POST",
@@ -31,6 +32,8 @@ const Login = () => {
       navigate("/home");
     } catch (error) {
       setErro(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -44,30 +47,41 @@ const Login = () => {
         />
       </div>
 
-      <h2 className="text-gray-300 text-xl font-medium mb-6">Acessar sua conta</h2>
+      <h2 className="text-gray-300 text-xl font-medium mb-6">
+        Acessar sua conta
+      </h2>
 
       <div className="flex flex-col w-80 gap-4">
-       <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="p-3 rounded-md bg-gray-200 focus:outline-none placeholder-[#534C4C]"
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="p-3 rounded-md bg-gray-200 text-gray-700 focus:outline-none placeholder-[#534C4C]"
         />
         <input
-            type="password"
-            placeholder="Senha"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            className="p-3 rounded-md bg-gray-200 focus:outline-none placeholder-[#534C4C]"
+          type="password"
+          placeholder="Senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          className="p-3 rounded-md bg-gray-200 text-gray-700 focus:outline-none placeholder-[#534C4C]"
         />
 
         <Button
-          className="mt-2 bg-orange-500 text-white py-3 rounded-md hover:bg-orange-600 text-lg"
+          className="mt-2 bg-orange-500 text-white py-3 rounded-md hover:bg-orange-600 text-lg flex justify-center items-center"
           onClick={handleLogin}
+          disabled={loading}
         >
-          Login
+          {loading ? (
+            <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin" />
+          ) : (
+            "Login"
+          )}
         </Button>
+
+        {erro && (
+          <p className="text-red-300 text-sm text-center mt-2">{erro}</p>
+        )}
       </div>
     </div>
   );

9b837c650d09bfbb5842b8f6342ff6d792a06fdd
Renan Bordignon Poy <renanbp2@gmail.com>
2025-09-25 12:34:10 -0300

    add user location

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,16 +16,37 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
 
-
+const userIcon = L.icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+});
 
 const HomePage = () => {
   const navigate = useNavigate();
   const [obras, setObras] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [userPosition, setUserPosition] = useState(null);
 
 
 useEffect(() => {
   let cancel = false;
+
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          if (!cancel) {
+            setUserPosition([pos.coords.latitude, pos.coords.longitude]);
+          }
+        },
+        (err) => {
+          console.warn("Erro ao obter localização do usuário:", err);
+        }
+      );
+  }
 
   const fetchData = async () => {
     setLoading(true);
@@ -72,9 +93,15 @@ useEffect(() => {
         >
           <TileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
 
+{userPosition && (
+  <Marker position={userPosition} icon={userIcon}>
+    <Popup>Você está aqui</Popup>
+  </Marker>
+)}
 
 {obras
   .filter((obra) => obra.latitude && obra.longitude)
+
   .map((obra) => (
     <Marker
       key={obra.id}

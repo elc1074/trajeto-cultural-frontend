0b263bfd878628b0780a02272c631ca005a43130
Dionatan Rodrigues <dionatanrodrigues351@gmail.com>
2025-10-23 16:28:04 -0300

    📦 chore : add vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,66 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 import path from "path";
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Trajeto Cultural",
+        short_name: "Trajeto",
+        description:
+          "Aplicativo cultural progressivo ! Explore, aprenda e descubra.",
+        theme_color: "#0a74da",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/maskable-icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images-cache",
+              expiration: { maxEntries: 60, maxAgeSeconds: 30 * 24 * 60 * 60 },
+            },
+          },
+          {
+            urlPattern: ({ url }) =>
+              url.origin === "https://trajeto-cultural-frontend.vercel.app",
+            handler: "NetworkFirst",
+            options: { cacheName: "api-cache" },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
